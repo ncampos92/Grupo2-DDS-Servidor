69519c30e13c8df0125c689cc4516170ca470a9b
ncampos92 <nlcampos@uc.cl>
2015-10-13 22:13:20 -0300

    métodos principales definidos

--- servidor/app/controllers/application_controller.rb
+++ servidor/app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #Verifica la autenticación de los usuarios.
+  def check_authenticated(user_id, user_token)
+  end
+
+  #Verifica el nivel del usuario para determinar los permisos que tiene.
+  def check_user_level(user_id)
+  end
 end
--- servidor/app/controllers/users_controller.rb
+++ servidor/app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  #Realiza acciones para autenticar un usuario.
+  def authenticate
+  end
+
   # GET /users
   # GET /users.json
   def index
