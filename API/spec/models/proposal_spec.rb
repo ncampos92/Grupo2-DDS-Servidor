9a046bff1915c850449a7987a14c9f62aaaf4e7d
Jurgen Heysen <jheysen@gmail.com>
2015-11-17 23:51:04 -0300

    Nuevo test

--- API/spec/models/proposal_spec.rb
+++ API/spec/models/proposal_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Proposal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should create" do
+  	@user = User.new
+  	@user.first_name = "Test"
+  	@user.last_name = "tester"
+  	@user.email = "test@testing.com"
+  	@user.password = "passwordtest123"
+  	@user.nivel_acceso = "admin"
+  	@user.save 
+  	@proposal = Proposal.new
+  	@proposal.user = @user
+  	@proposal.texto = "Esto es un test"
+  	@proposal.titulo = "Test"
+  	@proposal.save!
+  	expect(Proposal.all.count).to eq(1)
+  end
 end
